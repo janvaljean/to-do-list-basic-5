fa8fb4a7a3dbe52bc5f621148d339f8ba8d77b58
janvaljean <kurtuldu@gmx.de>
2023-01-31 16:20:34 +0100

    todo app done

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 var items = ["item1", "item2", "item3", "item4"];
-var list = document.createElement("ul");
-document.querySelector(".card-header").appendChild(list);
-list.className = "list-group";
+
+
+var list = document.querySelector("#myList");
 
 
 items.forEach(function (item) {
-    list.classList.add("list-group-flush");
-    list.id = "myList";
+
+    CreateItem(item)
+
+})
+
+   
+
+list.addEventListener("click", function (item) {
+   
+    if (item.target.tagName == "LI") {
+        item.target.classList.toggle("checked")
+        ToogleDeleteButton()
+
+        }
+    })
+document.querySelector("#deleteAll").onclick = function () {
+    var elements =  document.querySelectorAll(".checked")
+    elements.forEach(function (element) {
+        element.style.display = "none"
+        
+    })
+}
+
+function ToogleDeleteButton() {
+    var checkList = document.querySelectorAll(".checked")
+    if (checkList.length > 0) {
+        document.querySelector("#deleteAll").classList.remove("d-none")
+
+    }   else {document.querySelector("#deleteAll").classList.add("d-none")
+    } 
+}
+
+document.querySelector("#btnCreate").onclick = function(){
+    var item = document.querySelector("#txtItem").value;
+    if (item === "") {
+        alert ("Please enter an item");
+
+        return;
+
+    }
+    CreateItem(item)
+    document.querySelector("#txtItem").value = "";
+
+}
+
+
+function CreateItem(item){
     var li = document.createElement("li");
     //! appended items name as a text to list
 
@@ -21,13 +66,13 @@ items.forEach(function (item) {
     span.className = "close";
     span.appendChild(text);
     li.appendChild(span);
-    
-    
+
+    span.onclick = function () {
+        var li = this.parentElement;
+        li.style.display = "none";
+        li.classList.remove("checked");
 
 
+    }
 
-
-    
-
-
-    })
+}
